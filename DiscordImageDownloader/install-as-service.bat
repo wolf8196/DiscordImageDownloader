9a1fdbe50b387fc515cb513a7d3e4d9be4090aa5
Roman Vovk <wolf8196@gmail.com>
2021-06-30 11:58:31 +0300

    fix install script

--- DiscordImageDownloader/install-as-service.bat
+++ DiscordImageDownloader/install-as-service.bat
@@ -1,5 +1,5 @@
 sc delete "Discord Image Downloader"
-sc create "Discord Image Downloader" start=auto binpath=%~dp0DiscordImageDownloader.exe
+sc create "Discord Image Downloader" start=auto binpath="%~dp0DiscordImageDownloader.exe"
 sc description "Discord Image Downloader" "Small and simple app to download images from Discord channels"
 sc start "Discord Image Downloader"
 pause
